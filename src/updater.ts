49b580d61dd64a5937c2b8469bf990794f36df1e
Rick Slijkhuis <rick.slijkhuis@wearetriple.com>
2024-06-26 16:49:04 +0200

    Remove message

--- src/updater.ts
+++ src/updater.ts
@@ -1,5 +1,5 @@
 import { check } from "@tauri-apps/plugin-updater";
-import { ask, message } from "@tauri-apps/plugin-dialog";
+import { ask } from "@tauri-apps/plugin-dialog";
 import { relaunch } from "@tauri-apps/plugin-process";
 
 export async function checkForAppUpdates() {
